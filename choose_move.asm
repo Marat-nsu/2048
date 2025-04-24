d6f2e726731850343102b9fea39633a7298d00d4
katiysheck <e.maksunova@g.nsu.ru>
2025-04-25 01:57:35 +0700

    Update choose_move.asm

--- choose_move.asm
+++ choose_move.asm
@@ -4,38 +4,32 @@ rsect choose_move
 
 move_matrix>
     #r0 - address of needed matrix
-    #r1 - address of main matrix
-    ldi r1, 0xff00
-    ldi r2, 0xff10
-    ldi r3, 0xff20
-    ldi r4, 0xff30
-    ldi r5, 0xff40
-    ldi r6, 0
+    #r2 - address of main matrix
+    ldi r6, 0x10
+    add r0, r6, r1
+    ldi r2, 0xff00
     while
-        cmp r6, 0x10
+        cmp r0, r1
     stays lt
-        ldb r0, r6, r7
-        stb r1, r6, r7
-        ldi r7, 0
-        # Очищаем поля
-        stb r2, r6, r7
-        stb r3, r6, r7
-        stb r4, r6, r7
-        stb r5, r6, r7
+        ldb r0, r3
+        stb r2, r3
 
-        add r6, 1
+        inc r0
+        inc r2
     wend
     rts
 
 
 choose_move>
 
+    # ldw
+    # stw
+
     # find max
     ldi r0, 0
     ldi r1, 0xff50
     ldw r1, r0, r3 #maximum
-    ldi r4, 0xff10 # Поле с максимальной оценкой
-    ldi r5, 0xff10 # Поле для текущей оценки
+    move r1, r4    #address of max value
     while
         cmp r0, 4
     stays lt
@@ -44,15 +38,45 @@ choose_move>
         if
             cmp r3, r2
         is lt
-            move r5, r4 #address of max value
+            inc r4 
+            move r2, r3
         fi
-        add r1, 2
-        add r5, 0x10
+        add r1, 1
         inc r0
     wend
 
-    move r4, r0
-    jsr move_matrix
 
+
+    # choose address of needed matrix
+    ldi r1, 0xff50
+    if
+        cmp r1, r4
+    is z
+        # 0xff10 - 0xff20
+        ldi r0, 0xff10
+        jsr move_matrix
+    else 
+        if
+            inc r1
+            cmp r1, r4
+        is z
+            # 0xff20 - 0xff30
+            ldi r0, 0xff20
+            jsr move_matrix
+        else 
+            if
+                inc r1
+                cmp r1, r4
+            is z
+                # 0xff30 - 0xff40
+                ldi r0, 0xff30
+                jsr move_matrix
+            else
+                # 0xff40 - 0xff50
+                ldi r0, 0xff40
+                jsr move_matrix
+            fi
+        fi
+    fi
     rts
 end.
